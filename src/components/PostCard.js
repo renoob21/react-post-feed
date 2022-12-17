afa1e5e5009c94df48fcaca32bfd1991a7f4f407
renoob21 <reno.iqbalsah21@gmail.com>
2022-12-17 11:34:36 +0700

    add margins and some styling

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -4,15 +4,15 @@ import './PostCard.css'
 
 const PostCard = ({post, userId, userName}) => {
     return (
-        <div className="tl bg-light-gray dib br3 pa3 bw3 shadow-5 dim w-100">
+        <div className="tl bg-light-gray dib br3 pa3 bw3 shadow-5 w-100 mv3 mh2">
             <ul>
                 <li>
                     <img src={`https://robohash.org/${userName}`} alt="profpic" className="br-100 ba h3 w3 dib bg-gray avatar" />
                 </li>
                 <li>
                     <div>
-                        <h3 className="dispname">{userId}</h3>
-                        <h6 className="username">{`@${userName.toLowerCase()}`}</h6>
+                        <h3 className="dispname dim">{userId}</h3>
+                        <h6 className="username dim">{`@${userName.toLowerCase()}`}</h6>
                     </div>
                     <div>
                         <p>{post}</p>
